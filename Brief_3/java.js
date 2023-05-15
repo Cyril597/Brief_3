d3816c01adbc8d68044f47dd224ab957037367fa
Homatec <132045330+Cyril597@users.noreply.github.com>
2023-05-15 16:52:55 +0200

    Add files via upload

---
+++ Brief_3/java.js
@@ -0,0 +1,7 @@
+let compteur = document.getElementById("visite")     
+if (localStorage.getItem("compteur")==null)      
+localStorage.setItem("compteur",0);       
+let compteurvue = Number(localStorage.getItem("compteur"));      
+localStorage.setItem("compteur", compteurvue+1);       
+console.log(compteur.innerHTML)      
+compteur.innerHTML = compteurvue;
